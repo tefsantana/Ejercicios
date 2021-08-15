5c21271d1e20873b4d2b0fb1a2a5e487f1d4de1d
Estefanía Santana <83146564+tefsantana@users.noreply.github.com>
2021-08-15 12:34:55 -0300

    Add files via upload

---
+++ Calificaciones.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estefanía Santana
+ */
+public class Calificaciones {
+    public static void main(String[] args) {
+        /* Debes pedirle 3 calificaciones al usuario. 
+        Sacarás el promedio de las calificaciones y verificaremos si el promedio es mayor a 6. 
+        En caso de ser así, el programa deberá lanzar un mensaje explicando nuestro status académico.
+        */
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe la primera calificación: ");
+        int calificacion1 = sc.nextInt();
+        
+        System.out.println("Escribe la segunda calificación: ");
+        int calificacion2 = sc.nextInt();
+        
+        System.out.println("Escribe la tercera calificación: ");
+        int calificacion3 = sc.nextInt();
+        
+        float promedio = (calificacion1 + calificacion2 + calificacion3) / 3;
+                
+        if (promedio >= 6) {
+            System.out.println("Condición del alumno: APROBADO.");
+        } else {
+            System.out.println("Condición del alumno: DESAPROBADO.");
+        }
+        
+    }
+}
